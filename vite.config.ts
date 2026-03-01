20cf055bd1bb27ac4b0415ac4cc254e85a60bc68
Charlie Briones <charliebriones@gmail.com>
2026-03-01 20:09:12 +0800

    vite tailwind update

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
+import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
   base: "/react-local-todo/",
-  plugins: [react()],
+  plugins: [react(), tailwindcss()],
 });
