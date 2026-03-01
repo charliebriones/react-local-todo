3c49d3ea5e3e4ec7b972f5dfeeee2a489c759370
Charlie Briones <charliebriones@gmail.com>
2026-03-01 20:06:26 +0800

    fix vite config

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 
 export default defineConfig({
-  base: "/todo-app/",
+  base: "/react-local-todo/",
   plugins: [react()],
 });
